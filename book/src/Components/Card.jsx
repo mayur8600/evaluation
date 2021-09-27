a83a99d1430d57e5987bf2895393deb3f1a0457b
mayur8600 <mayur.kamthe62987@gmail.com>
2021-09-27 17:03:41 +0530

    styling

--- book/src/Components/Card.jsx
+++ book/src/Components/Card.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 function Card({data}) {
 
     return (
-        <div>
-            <p>{data.title}</p>
+        <div style={{border: '1px solid',
+        display: 'flex',
+        flexDirection:"row"}}>
+            <img src={data.img} alt="img" />
+            <p>Book Name<h3>{data.title}</h3></p>
+            <p>Anuther Name: {data.auther.name}</p>
 
         </div>
     )
