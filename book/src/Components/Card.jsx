d02af51e434b4f681a5d51f0d12447b478c5534f
mayur8600 <mayur.kamthe62987@gmail.com>
2021-09-27 16:09:49 +0530

    test

---
+++ book/src/Components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Card() {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Card
---
+++ book/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <input type="text"  placeholder="Enter Book Name"/>
+        </div>
+    )
+}
+
+export default Navbar
